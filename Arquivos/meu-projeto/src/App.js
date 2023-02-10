8e50526f3866c0c8db7c021fcca26b4dfb26b005
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-02-09 22:13:06 -0300

    entendendo o JSX

---
+++ Arquivos/meu-projeto/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const name = 'Mateus'
+
+  const newName = name.toUpperCase() /* Letra Maiúscula */
+
+  function sum(a, b)
+  {
+    return a + b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
+  return (
+    <div className="App">
+      <h2>Alternando o JSX</h2>
+      <p>Olá, {newName}</p>
+      <p>Soma: {sum(1, 2)}</p>
+      <img src={url} alt='Minha Imagem' />
+    </div>
+  )
+}
+
+export default App;
